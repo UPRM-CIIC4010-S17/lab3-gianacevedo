0a72f4554b09113b897650038ed53104d7cc6546
4015e1 <4015e1@anon.ece.uprm.edu>
2017-02-08 19:17:09 -0400

    MyPanelClass was created in order to create a panel that got added to the frame in the Lab3Exercises class. Two borders where created inside the frame with different colors.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,14 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Gian Acevedo");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 400);
+		myFrame.setLocation(900, 300);
 		myFrame.setSize(200, 200);
+		
+		//parte 6 del laboratorio
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+        //termina parte 6
+        
 		myFrame.setVisible(true);
 	}
 }
